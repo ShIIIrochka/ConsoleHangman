9c557090bd12d99556f33b9eb8071ac72e1e54c0
ShIIIrochka <shiroooooo@yandex.ru>
2024-12-06 17:18:03 +0300

    class Session add variables and colstructor

--- src/Session.java
+++ src/Session.java
@@ -1,3 +1,36 @@
-public class Session {
+import java.util.Scanner;
 
+public class Session {
+    /*
+    answer: word to guess
+    user_answer: user answer
+    max_attempts: max attempts to guess word
+    attemts: user attempts
+     */
+
+    private final String answer;
+    private final char[] user_answer;
+    private final int max_attempts;
+    private int attempts;
+
+    public Session(String answer, int max_attempts) {
+        this.answer = answer;
+        this.max_attempts = max_attempts;
+        this.user_answer = new char[answer.length()];
+        for (int i = 0; i < user_answer.length; i++) {
+            user_answer[i] = '_';
+        }
+        this.attempts = 0;
+    }
+
+
+    GuessResult guess(char guess) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Guess a letter: ");
+        String letter = scanner.nextLine();
+    }
+
+    GuessResult giveUp() {
+        return null;
+    }
 }
