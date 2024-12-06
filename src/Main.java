606027c45e6a9788ce1e0118d27b0f25e55009c7
ShIIIrochka <shiroooooo@yandex.ru>
2024-12-06 19:01:55 +0300

    add root file

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+        Dictionary dictionary = new SimpleDictionary();
+        ConsoleHangman game = new ConsoleHangman(dictionary);
+        game.run();
     }
 }
