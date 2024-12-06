3c84314f0e4cc4614e0cf7317c1ff7cdc7580ec6
ShIIIrochka <shiroooooo@yandex.ru>
2024-12-06 18:00:41 +0300

    codestyle

--- src/Session.java
+++ src/Session.java
@@ -2,55 +2,54 @@ import java.util.Scanner;
 
 public class Session {
     /*
-    answer: word to guess
-    user_answer: user answer
-    max_attempts: max attempts to guess word
-    attemts: user attempts
+     * answer: word to guess
+     * userAnswer: user answer
+     * maxAttempts: max attempts to guess word
+     * attempts: user attempts
      */
 
     private final String answer;
-    private final char[] user_answer;
-    private final int max_attempts;
+    private final char[] userAnswer;
+    private final int maxAttempts;
     private int attempts;
 
-    public Session(String answer, int max_attempts) {
+    public Session(String answer, int maxAttempts) {
         this.answer = answer;
-        this.max_attempts = max_attempts;
-        this.user_answer = new char[answer.length()];
-        for (int i = 0; i < user_answer.length; i++) {
-            user_answer[i] = '_';
+        this.maxAttempts = maxAttempts;
+        this.userAnswer = new char[answer.length()];
+        for (int i = 0; i < userAnswer.length; i++) {
+            userAnswer[i] = '_';
         }
         this.attempts = 0;
     }
 
-
-    GuessResult guess(char guess) {
+    public GuessResult guess(char guess) {
         boolean found = false;
 
         for (int i = 0; i < answer.length(); i++) {
             if (answer.charAt(i) == guess) {
-                user_answer[i] = guess;
+                userAnswer[i] = guess;
                 found = true;
             }
         }
         attempts++;
 
-        if (String.valueOf(user_answer).equals(answer)) {
-            return new GuessResult.Win(user_answer, attempts, max_attempts, "You won!");
+        if (String.valueOf(userAnswer).equals(answer)) {
+            return new GuessResult.Win(userAnswer, attempts, maxAttempts, "You won!");
         }
 
-        if (!found && attempts >= max_attempts) {
-            return new GuessResult.Defeat(user_answer, attempts, max_attempts, "You lost!");
+        if (!found && attempts >= maxAttempts) {
+            return new GuessResult.Defeat(userAnswer, attempts, maxAttempts, "You lost!");
         }
 
         if (found) {
-            return new GuessResult.SuccessfulGuess(user_answer, attempts, max_attempts, "Hit!");
+            return new GuessResult.SuccessfulGuess(userAnswer, attempts, maxAttempts, "Hit!");
         } else {
-            return new GuessResult.FailedGuess(user_answer, attempts, max_attempts, "Missed");
+            return new GuessResult.FailedGuess(userAnswer, attempts, maxAttempts, "Missed");
         }
     }
 
     public GuessResult giveUp() {
-        return new GuessResult.Defeat(user_answer, attempts, max_attempts, "You lost!");
+        return new GuessResult.Defeat(userAnswer, attempts, maxAttempts, "You lost!");
     }
 }
